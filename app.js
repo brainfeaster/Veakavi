d9b3c9768c5de112efebd07d4190c03f8fa05f9e
Sanjai <Sanjai.com@gmail.com>
2021-09-21 22:12:04 -0400

    all done

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ Vue.createApp({
     data() {
         return {
             goals: [],
-            enteredValue:''
+            enteredValue: '',
+            homePageLnk: 'https://sanjaipk.github.io',
+            htmlcontent: '<b>hello bolding</b>'
         };
     },
     methods: {
         addGoal() {
             this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        },
+        getRandomNumber() {
+            return Math.random();
         }
     }
 }).mount('#app');
